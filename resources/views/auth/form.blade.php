994dd93ec955cd4d1675bb52e0be0fa81c0bef24
attakei <attakei@gmail.com>
2016-02-26 02:34:48 +0900

    Use common layout for pages

--- resources/views/auth/form.blade.php
+++ resources/views/auth/form.blade.php
@@ -1,71 +1,21 @@
+@extends('_layout.page')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Navbar Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap-theme.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="/css/navbar.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-<div class="container">
-
-    <!-- Static navbar -->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-    </nav>
-
-    <!-- Main component for a primary marketing message or call to action -->
-    <div class="jumbotron">
-        <h2>認証</h2>
-        <form method="post" style="width:50%;">
-            <div class="form-group">
-                <label for="authEmail">Email address</label>
-                <input type="email" class="form-control" id="authEmail" name="email" placeholder="Email">
-            </div>
-            <button type="button" class="btn btn-default" id="submitAuth">サインアップ/ログイン</button>
-        </form>
-    </div>
-
-</div> <!-- /container -->
+@section('main')
+<!-- Main component for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <h2>認証</h2>
+    <form method="post" style="width:50%;">
+        <div class="form-group">
+            <label for="authEmail">Email address</label>
+            <input type="email" class="form-control" id="authEmail" name="email" placeholder="Email">
+        </div>
+        <button type="button" class="btn btn-default" id="submitAuth">サインアップ/ログイン</button>
+    </form>
+</div>
+@endsection
 
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.0/js.cookie.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+@section('js')
 <script>
     $('#submitAuth').click(function(event){
         var formData = {};
@@ -99,5 +49,4 @@
         });
     });
 </script>
-</body>
-</html>
+@endsection
