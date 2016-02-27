9f6a08e809ce9898eb984f4aeb40f2ab6f8e4fe0
attakei <attakei@gmail.com>
2016-02-27 11:57:08 +0900

    Add current top page

---
+++ resources/views/top/index.blade.php
@@ -0,0 +1,40 @@
+@extends('_layout.page')
+
+@section('main')
+    <div class="container">
+
+        <!-- Main component for a primary marketing message or call to action -->
+        <div class="jumbotron">
+            <h1>PHP Qiita-ism</h1>
+            <p>Mini Qiita clone for small team written by PHP and Laravel</p>
+        </div>
+
+    </div> <!-- /container -->
+
+    <div class="container">
+
+        <!-- Main component for a primary marketing message or call to action -->
+        <div class="row">
+            <h2>Login / SignUp</h2>
+            <div class="col-sm-offset-1 col-sm-5">
+                <form>
+                    Google アカウントでログイン
+                </form>
+            </div>
+            <div class="col-sm-6">
+                @include('_snippet.auth.login')
+            </div>
+        </div>
+    </div> <!-- /container -->
+
+@endsection
+
+
+@section('js')
+    <script type="text/javascript">
+        $('#authSubmit').click(function (event) {
+            event.target.innerHTML = 'ログイン中';
+            return false;
+        });
+    </script>
+@endsection
