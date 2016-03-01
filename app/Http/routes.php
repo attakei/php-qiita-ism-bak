4c5806af91247ae649a90e55e7a969b0883baadb
attakei <attakei@gmail.com>
2016-03-01 16:35:08 +0900

    Merge same routing groups

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,12 +27,9 @@ Route::group(['middleware' => ['web']], function () {
         return view('top.index');
     });
 
+    Route::auth();
     Route::post('/auth/google', ['as' => 'auth_oauth_google', 'uses' => 'Auth\AuthController@redirectToProvider']);
     Route::get('/auth/google/callback', ['as' => 'auth_oauth_callback', 'uses' => 'Auth\AuthController@callbackFromProvider']);
-});
-
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
 
     Route::get('/dashboard', 'HomeController@dashboard');
 });
