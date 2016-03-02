78ce37a51118b1462eded6e06c49671dfc4be968
attakei <attakei@gmail.com>
2016-03-02 20:08:01 +0900

    Rename migration file

--- database/migrations/2016_03_01_102615_init_articles.php
+++ database/migrations/2016_03_01_102615_init_articles.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class InitArtigles extends Migration
+class InitArticles extends Migration
 {
     /**
      * Run the migrations.
