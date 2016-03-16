dd1ab1bc231e5a41c979f92d9f0e0e1c2db16af8
attakei <attakei@gmail.com>
2016-03-16 17:35:14 +0900

    [WIP] パスフレーズ対応：CircleCIからデプロイしてもらうテスト

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,4 @@
 #!/bin/sh
-eval `ssh-agent`
-
-echo $DOKKU_DEPLOY_KEY >> ~/.ssh/dokku_deploy_key
-chmod 400 ~/.ssh/dokku_deploy_key
-
-cat << EOS >> ~/.ssh/config
-
-Host lab.attakei.net
-  User dokku
-  Port 22
-  IdentityFile ~/.ssh/dokku_deploy_key
-  IdentitiesOnly no
-  StrictHostKeyChecking no
-  UserKnownHostsFile=/dev/null
-
-EOS
-
 
 git remote add dokku dokku@lab.attakei.net:qiita
 git push dokku master
